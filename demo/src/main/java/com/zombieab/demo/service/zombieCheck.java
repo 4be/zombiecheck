e89893545453a82315f41c6cc8e3eb879084c79e
Abe <jaderizan@gmail.com>
2022-11-07 17:22:56 +0700

    test

--- demo/src/main/java/com/zombieab/demo/service/zombieCheck.java
+++ demo/src/main/java/com/zombieab/demo/service/zombieCheck.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class zombieCheck {
 
-    public static void main(String[] args) {
+    private void main(String[] args) {
 
     }
 
